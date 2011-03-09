28ca6c3882e38f306cd42a4adaadfb9ae2e011c6
Piers Harding <piers@catalyst.net.nz>
2011-03-09 16:15:09 +1300

    Moved files into auth/saml to make it easier for install

---
+++ auth/saml/config.php
@@ -0,0 +1,26 @@
+<?php 
+/**
+ * config.php - config file for auth/saml based SAML 2.0 login
+ * 
+ * make sure that you define both the SimpleSAMLphp lib directory and config
+ * directory for the associated SP and also specify the IdP that it will talk to
+ * 
+ * 
+ * 
+ * @originalauthor Martin Dougiamas
+ * @author Erlend Strømsvik - Ny Media AS 
+ * @author Piers Harding - made quite a number of changes
+ * @version 1.0
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package auth/saml
+ */
+
+
+$SIMPLESAMLPHP_LIB = '/var/simplesamlphp';
+$SIMPLESAMLPHP_CONFIG = '/var/simplesamlphp/config';
+$SIMPLESAMLPHP_SP = 'default-sp';
+
+// change this to something specific if you don't want users to be sent to
+// Moodle $CFG->wwwroot when logout is completed
+$SIMPLESAMLPHP_LOGOUT_LINK = "";  
+?>
